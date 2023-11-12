fbec17bc85cf1fa67b33db562734bf9080abb76c
Fabien Leroy <fabienleroy2004@gmail.com>
2023-11-12 23:14:05 +0100

    corection

--- view/template.php
+++ view/template.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>P'tit Cuisto</title>
+    <title>PtiCuisto</title>
     <link href="../../css/bootstrap.css" rel="stylesheet">
     <link href="../../css/style.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="../../images/icon.png">
