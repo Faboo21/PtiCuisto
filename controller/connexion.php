765e1eb153689711c172a80d2538864af3d674cc
Faboo21 <fabienleroy2004@gmail.com>
2023-11-13 02:38:50 +0100

    VFinale

--- controller/connexion.php
+++ controller/connexion.php
@@ -11,19 +11,16 @@ $stmt = $logIN->connexionValide($_GET["emailC"]);
 $ban = $logIN->bannis($_GET["emailC"]);
 
 
-if (isset($stmt[0])) {
-    if (password_verify($_GET["passwordC"], $stmt[0]['UTI_Mdp']) && !empty($_GET["emailC"]) && !empty($_GET["passwordC"])) {
-        if ($ban[0]['uti_Statut'] == 0){
-            $_SESSION["connexion_rate"] = 'ban';
-        } else {
-            $stmt = $logIN->getPseudo($_GET["emailC"]);
+if (password_verify($_GET["passwordC"], $stmt[0]['UTI_Mdp']) && !empty($_GET["emailC"]) && !empty($_GET["passwordC"])) {
+    if ($ban[0]['uti_Statut'] == 0) {
+        $_SESSION["connexion_rate"] = 'ban';
+    } else {
+        $stmt = $logIN->getPseudo($_GET["emailC"]);
         $_SESSION["user"] = $stmt[0]['uti_pseudo'];
-        $_SESSION["isadmin"] = $stmtadmin[0]['typeUser']; 
-        }
-        header('Location: ../view/page/edito.php');
+        $_SESSION["isadmin"] = $stmtadmin[0]['typeUser'];
     }
-}
-else {
+    header('Location: ../view/page/edito.php');
+} else {
     if (empty($_GET["emailC"]) || empty($_GET["passwordC"])) {
         $_SESSION["connexion_rate"] = 'vide';
     } else {
@@ -31,4 +28,3 @@ else {
     }
     echo '<script>window.history.back();</script>';
 }
-?>
