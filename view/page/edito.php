c54052216831400fd5ffcb88c91d54fb0dfbfb95
Faboo <fabienleroy2004@gmail.com>
2023-11-13 10:21:36 +0100

    design edito

--- view/page/edito.php
+++ view/page/edito.php
@@ -36,56 +36,51 @@ session_start(); ?>
             <span class="sr-only">Suivant</span>
         </a>
     </div>
+<div class="row">
+    <!-- Zone des dernières recettes -->
+    <div class="col-md-6 border border-primary">
+        <h1 class="titre mt-2">Les dernières recettes</h1>
+        <ul class="list-group list-group-flush">
+            <?php
+            require_once '../../model/RecipeManager.php';
 
-    <!-- Contenu en dessous de l'image -->
-    <div class="row">
-        <!-- Zone des dernières recettes -->
-        <div class="col-md-5 border border-primary m-5"> <!-- Réduire la largeur à col-md-5 -->
-            <h1 class="titre mt-2">Les dernières recettes</h1>
-            <ul class="list-group list-group-flush">
-                <?php
-                require_once '../../model/RecipeManager.php';
+            $recipe = new RecipeManager();
+            $recipes = $recipe->afficheTroisRecette();
 
-                $recipe = new RecipeManager();
-                $recipes = $recipe->afficheTroisRecette();
-
-                if (is_array($recipes) && !empty($recipes)) {
-                    foreach ($recipes as $row) {
-                        echo '<li class="list-group-item">';
-                        echo '<a href="../../controller/detail.php?id=' . $row['REC_Id'] . '" class="text-decoration-none text-dark">';
-                        echo '<div class="row mb-4">';
-                        echo '<div class="col-md-4 col-12">'; // Pour que cela prenne toute la largeur sur mobile
-                        // Ajoutez des classes responsives à l'image pour la redimensionner
-                        echo '<img src="../../images/image_recette/' . $row['REC_Image'] . '" alt="Image_' . $row['REC_Titre'] . '" class="img-fluid">';
-                        echo '</div>';
-                        echo '<div class="col-md-8 col-12 texte">'; // Pour que cela prenne toute la largeur sur mobile
-                        echo '<h2>' . $row['REC_Titre'] . '</h2>';
-                        echo '<p>' . $row['REC_Resum'] . '</p>';
-                        echo '</div>';
-                        echo '</div>';
-                        echo '</a>';
-                        echo '</li>';
-                    }
-                } else {
-                    echo '<li class="list-group-item"> Aucune recette trouvée. </li>';
+            if (is_array($recipes) && !empty($recipes)) {
+                foreach ($recipes as $row) {
+                    echo '<li class="list-group-item">';
+                    echo '<a href="../../controller/detail.php?id=' . $row['REC_Id'] . '" class="text-decoration-none text-dark">';
+                    echo '<div class="row mb-4">';
+                    echo '<div class="col-md-4 col-12">';
+                    echo '<img src="../../images/image_recette/' . $row['REC_Image'] . '" alt="Image_' . $row['REC_Titre'] . '" class="img-fluid">';
+                    echo '</div>';
+                    echo '<div class="col-md-8 col-12 texte">';
+                    echo '<h2>' . $row['REC_Titre'] . '</h2>';
+                    echo '<p>' . $row['REC_Resum'] . '</p>';
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</a>';
+                    echo '</li>';
                 }
-                ?>
-            </ul>
+            } else {
+                echo '<li class="list-group-item"> Aucune recette trouvée. </li>';
+            }
+            ?>
+        </ul>
+    </div>
 
-        </div>
+    <!-- Zone d'édito -->
+    <div class="col-md-6 bg-primary p-4">
+        <!-- Image centrée dans l'édito -->
+        <img src="../../images/edito.png" alt="Image de l'édito" class="editorial-img mb-4">
 
-        <!-- Zone d'édito -->
-        <div class="col-md-6 bg-primary p-4"> <!-- Réduire la largeur à col-md-6 -->
-            <!-- Image centrée dans l'édito -->
-            <img src="../../images/edito.png" alt="Image de l'édito" class="editorial-img mb-4">
-
-            <!-- Titre et texte de l'édito -->
-            <div class="editorial-content text-light">
-                <h2 class="titre">Édito</h2>
-                <p class="texte">
-                    Bienvenue sur notre site de cuisine ! Ici, vous découvrirez une multitude de recettes alléchantes, des astuces de chefs et des conseils pour rendre chaque repas exceptionnel. Que vous soyez débutant ou cuisinier chevronné, notre site est conçu pour vous inspirer et enrichir votre passion pour la cuisine. Plongez dans nos contenus et laissez-vous guider par les saveurs !
-                </p>
-            </div>
+        <!-- Titre et texte de l'édito -->
+        <div class="editorial-content text-light">
+            <h2 class="titre">Édito</h2>
+            <p class="texte">
+                Bienvenue sur notre site de cuisine ! Ici, vous découvrirez une multitude de recettes alléchantes, des astuces de chefs et des conseils pour rendre chaque repas exceptionnel. Que vous soyez débutant ou cuisinier chevronné, notre site est conçu pour vous inspirer et enrichir votre passion pour la cuisine. Plongez dans nos contenus et laissez-vous guider par les saveurs !
+            </p>
         </div>
     </div>
 </div>
